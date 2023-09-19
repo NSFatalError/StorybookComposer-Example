f786c135cb7fae0f0fa69441d3cb2204d5b6e41f
Kamil Strzelecki <strzeleckikamil.developer@gmail.com>
2023-09-19 20:35:58 +0200

    Initial Commit

---
+++ StorybookComposer-Example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StorybookComposer-Example
+//
+//  Created by Kamil Strzelecki on 19/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StorybookComposer-Example/StorybookComposer_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StorybookComposer_ExampleApp.swift
+//  StorybookComposer-Example
+//
+//  Created by Kamil Strzelecki on 19/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct StorybookComposer_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
